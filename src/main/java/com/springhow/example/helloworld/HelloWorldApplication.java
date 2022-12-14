895a2ec04be6952c2520f1088f72d7fb8a60015c
himank328 <himanshu.kapoor@zemosolabs.com>
2022-12-14 10:23:00 +0530

    second commitsfdgfd

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "hello this is trigger by jenkins........";
+        return "hello this is trigger........";
     }
 
 }
